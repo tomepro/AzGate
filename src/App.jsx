c68abb07d90d07e60863318e049357eb2c33b4f6
uri <uri@uri.com>
2025-03-17 18:50:53 +0100

    Added frontend for Login Screen

--- src/App.jsx
+++ src/App.jsx
@@ -16,37 +16,25 @@ function App() {
 
   return (
     <main className="container">
-      <h1>{t("welcome")}</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="loginBox backdrop-blur-md">
+        <p className="loginBoxTitle">{t("login")}</p>
+        <div className="formDiv">
+          <form>
+            <input type="text" placeholder={t("username")} id="username"></input>
+            <input type="text" placeholder={t("password")} id="password"></input><br/>
+            <div className="formLabels">
+              <div className="checkboxDiv"><input type="checkbox"/> <label className="checkboxLabel">{t("rememberme")}</label></div>
+              <div className="forgotDiv"><a href="#">{t("forgotpasswd")}</a></div>
+            </div>
+            <button type="submit" className="loginButton">{t("login_button")}</button>
+          </form>
+          <div className="noAccountDiv">
+            <p>{t("dontHaveAnAccount")}</p><a href="#">{t("signup")}</a>
+          </div>
+        </div>
       </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
     </main>
+    
   );
 }
 
