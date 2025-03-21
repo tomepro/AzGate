b1c34b13ab3123c4b119581795e7505cf397de60
uri <uri@uri.com>
2025-03-21 17:40:55 +0100

    Defined log_in_request function

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,11 +4,17 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn log_in_request(_username: &str, _password: &str) -> bool {
+    return false;
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![log_in_request])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
