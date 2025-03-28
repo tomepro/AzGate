37caff3dad7cc9b48e816277e59a3b0630b2ef24
uri <uri@uri.com>
2025-03-28 18:31:48 +0100

    Changed backend structure and added .env file

---
+++ src-tauri/src/auth.rs
@@ -0,0 +1,79 @@
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+use dotenvy::dotenv;
+use std::env;
+
+#[derive(Serialize)]
+struct AuthRequest {
+    username: String,
+    password: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(dead_code)]
+#[serde(untagged)] // Allows handling multiple response types
+enum AuthResponse {
+    Success {
+        status: String,
+        token: String,
+        account: Account,
+    },
+    Error {
+        status_code: u16,
+        message: Vec<String>,
+        error: String,
+    },
+}
+
+#[derive(Debug, Deserialize)]
+struct Account {
+    id: u32,
+    username: String,
+    reg_mail: String,
+}
+
+#[tauri::command]
+pub async fn log_in_request(username: String, password: String) -> Result<serde_json::Value, String> {
+    dotenv().ok();
+    let client = Client::new();
+
+    let login_data = AuthRequest {
+        username,
+        password,
+    };
+    
+    
+    let api_url = env::var("API_URL").map_err(|err| err.to_string())? + "/auth/signin";
+
+    let response = client
+        .post(&api_url)
+        .json(&login_data)
+        .send()
+        .await
+        .map_err(|err| err.to_string())?;
+    
+    #[allow(unused_variables)]
+    let status = response.status();
+    let body = response.json::<AuthResponse>().await.map_err(|err| err.to_string())?;
+
+    match body {
+        AuthResponse::Success { token, account, .. } => {
+            Ok(json!({
+                "status": "success",
+                "token": token,
+                "account": {
+                    "id": account.id,
+                    "username": account.username,
+                    "reg_mail": account.reg_mail
+                }
+            }))
+        }
+        AuthResponse::Error { message, .. } => {
+            Ok(json!({
+                "status": "error",
+                "message": message.join(", ")
+            }))
+        }
+    }
+}
