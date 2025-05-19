43b728849a52c4b13b10a0ddcb28f0d1afd261f8
tomepro <marctomjim@campus.monlau.com>
2025-05-19 19:33:34 +0200

    problem use api

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,31 +1,13 @@
 mod api;
 mod game;
-use api::changelog::fetch_changelog;
-use api::json::crear_json_vacio;
-use api::json::save_version_to_file;
-use api::jwt::{delete_jwt, get_jwt, save_jwt};
-use api::login::log_in_request;
-use api::news::fetch_news;
-use api::profile::fetch_profile;
-use api::realms::fetch_realms;
-use api::register::register_user;
-use api::json::get_all_versions;
-use api::account_points::fetch_coins;
-use api::json::launch_version;
-use api::send_password_reset::send_password_email;
-use api::json::update_version;
-use api::json::delete_version;
-use api::list_addons::list_addons;
-use api::list_addons::open_folder;
-use api::list_addons::unzip_and_move;
-
-use api::reset_password::reset_password;
-
-use api::login::jwt_login;
 use game::version::get_version;
 use std::path::Path;
 
 use tauri_plugin_fs::init;
+
+use api::list_addons::list_addons;
+use api::list_addons::unzip_and_move;
+use api::list_addons::open_folder;
 
 // --- API Modules ---
 use api::{
@@ -47,8 +29,6 @@ use api::{
     shop::fetch_shop_items,
     tickets,
 };
-
-use game::version::get_version;
 
 // --- Entry Point ---
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
