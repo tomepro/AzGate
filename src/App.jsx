0533fb044674ce1a8128dfabd5a49c1ad22720fc
tomepro <marctomjim@campus.monlau.com>
2025-05-20 16:01:22 +0200

    fix shop

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,13 @@ function App() {
         <Route path="/registerScreen" element={<RegisterScreen />} />
         <Route path="/passwdRecoveryScreen" element={<PasswdRecoveryScreen />} />
         <Route path="/home" element={<HomeScreen />} />
+        <Route path="/shopScreen" element={<ShopScreen />} />
         <Route path="/armoryScreen" element={<ArmoryScreen />} />
         <Route path="/rankingScreen" element={<RankingScreen />} />
         <Route path="/addonsScreen" element={<AddonsScreen />} />
         <Route path="/changelogScreen" element={<ChangelogScreen />} />
         <Route path="/newsScreen" element={<NewsScreen />} />
+        <Route path="/shoppingCartScreen" element={<ShoppingCartScreen />} />
       </Routes>
     </Router>
   );
